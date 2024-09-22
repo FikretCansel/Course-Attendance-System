1589979888b2aec83c245bafa29cae3baec21f9f
Fikret Cansel <f.cansel@featuremind.com>
2024-09-22 15:02:31 +0300

    course update sayfası oluşturuldu

---
+++ src/app/courses/update/[id]/page.tsx
@@ -0,0 +1,10 @@
+import CoursesComponent from "@/components/CoursesComponent";
+
+export default function CourseUpdate() {
+  return (
+    <div>
+        Update componenti olacak
+      <CoursesComponent/>
+    </div>
+  );
+}
